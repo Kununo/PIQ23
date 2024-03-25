1668092097e612f63f33fba773562900a25b50be
Kununo <1633024580@qq.com>
2024-03-25 21:31:31 +0800

    first commit

--- train_test_IQA.py
+++ train_test_IQA.py
@@ -2,8 +2,11 @@ import os
 import argparse
 import random
 import numpy as np
+import torch
+
 from HyerIQASolver import HyperIQASolver
 
+np.float = np.float32
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
@@ -11,13 +14,15 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 def main(config):
 
     folder_path = {
-        'live': '/home/ssl/Database/databaserelease2/',
+        'live': '/home/pan/dataset/databaserelease2/',
         'csiq': '/home/ssl/Database/CSIQ/',
         'tid2013': '/home/ssl/Database/TID2013/',
         'livec': '/home/ssl/Database/ChallengeDB_release/ChallengeDB_release/',
         'koniq-10k': '/home/ssl/Database/koniq-10k/',
         'bid': '/home/ssl/Database/BID/',
+        'PIQ23': '/home/pan/dataset/Dataset',
     }
+
 
     img_num = {
         'live': list(range(0, 29)),
@@ -26,6 +31,7 @@ def main(config):
         'livec': list(range(0, 1162)),
         'koniq-10k': list(range(0, 10073)),
         'bid': list(range(0, 586)),
+        'PIQ23': list(range(0, 5112)),
     }
     sel_num = img_num[config.dataset]
 
@@ -41,7 +47,11 @@ def main(config):
         test_index = sel_num[int(round(0.8 * len(sel_num))):len(sel_num)]
 
         solver = HyperIQASolver(config, folder_path[config.dataset], train_index, test_index)
-        srcc_all[i], plcc_all[i] = solver.train()
+        srcc_all[i], plcc_all[i], model_paras, train_loss = solver.train()
+        if train_loss < min_loss:
+            min_loss = train_loss
+            torch.save(model_paras, 'best_jod.plk')
+
 
     # print(srcc_all)
     # print(plcc_all)
@@ -53,18 +63,19 @@ def main(config):
     # return srcc_med, plcc_med
 
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', dest='dataset', type=str, default='livec', help='Support datasets: livec|koniq-10k|bid|live|csiq|tid2013')
+    parser.add_argument('--dataset', dest='dataset', type=str, default='PIQ23', help='Support datasets: livec|koniq-10k|bid|live|csiq|tid2013')
     parser.add_argument('--train_patch_num', dest='train_patch_num', type=int, default=25, help='Number of sample patches from training image')
     parser.add_argument('--test_patch_num', dest='test_patch_num', type=int, default=25, help='Number of sample patches from testing image')
     parser.add_argument('--lr', dest='lr', type=float, default=2e-5, help='Learning rate')
     parser.add_argument('--weight_decay', dest='weight_decay', type=float, default=5e-4, help='Weight decay')
     parser.add_argument('--lr_ratio', dest='lr_ratio', type=int, default=10, help='Learning rate ratio for hyper network')
-    parser.add_argument('--batch_size', dest='batch_size', type=int, default=96, help='Batch size')
-    parser.add_argument('--epochs', dest='epochs', type=int, default=16, help='Epochs for training')
+    parser.add_argument('--batch_size', dest='batch_size', type=int, default=8, help='Batch size')
+    parser.add_argument('--epochs', dest='epochs', type=int, default=50, help='Epochs for training')
     parser.add_argument('--patch_size', dest='patch_size', type=int, default=224, help='Crop size for training & testing image patches')
-    parser.add_argument('--train_test_num', dest='train_test_num', type=int, default=10, help='Train-test times')
+    parser.add_argument('--train_test_num', dest='train_test_num', type=int, default=1, help='Train-test times')
 
     config = parser.parse_args()
     main(config)
